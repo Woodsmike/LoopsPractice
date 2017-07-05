b90a065fcc9c74a250bb83ec2f683df9d3383bfc
Woodsmike <Woodsmike@users.noreply.github.com>
2017-07-05 15:15:23 -0400

    beginning

--- LoopsPractice/Program.cs
+++ LoopsPractice/Program.cs
@@ -10,6 +10,21 @@ namespace LoopsPractice
     {
         static void Main(string[] args)
         {
+            //When would you use a loop
+            //Why would you use a loop
+            //nested loops
+            //infinite loops
+            //keywords, break and continue
+            //four types of loops {for loops, for each loops, while loops, do while loops}
+
+            //for loop   - both counting and Iterating through an array or other collection
+            //type
+            //for(initializer; condition, updater)
+            //{
+            //  do something
+            //}
+
+            
 
         }
     }
